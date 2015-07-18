1142509f14b507b7e3d9443f23faf8c164746b14
roidayan <roidayan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-18 14:50:42 +0000

    tag 1.5.3
    git-svn-id: https://plugins.svn.wordpress.org/simple-youtube/tags/1.5.3@1201653 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,76 @@
+=== Simple YouTube ===
+Contributors: roidayan
+Donate link: http://roidayan.com
+Tags: youtube, flv, player, playlist, video
+Requires at least: 3.2.1
+Tested up to: 4.2.2
+Stable tag: 1.5.3
+
+Simple youtube plugin to help embed youtube videos
+with playlist by time.
+
+== Description ==
+
+Simple youtube plugin to help embed youtube videos.
+
+Support for playlist by time (jump to specific time in the video).
+
+playlist css is from flowplayer playlist plugin.
+
+== Installation ==
+
+Extract to plugins directory.
+Use shortcode [youtube] to add vidoes.
+
+* example1:
+    `[youtube video='id']`
+	where id is the youtube video id.
+	
+* Example2:
+`	[youtube video='id' vparams='rel=0|border=0']
+	From the start, 0
+	Jump ahead,  1:30
+	Jump again, 2:04
+	[/youtube]`
+	
+Options for the shortcode:
+*   id - youtube video id.
+*	width - width of the video frame.
+*   height - height of the video frame.
+*	class - class of the playlist.
+*	side - side of the playlist. left or right.
+*   vparams - additional youtube video parameters.
+
+Available youtube video parameters can be found here:
+*    http://code.google.com/apis/youtube/player_parameters.html
+    
+== Frequently Asked Questions ==
+
+= How to modify the playlist style? =
+
+You need to edit playlist.css
+
+== Screenshots ==
+
+1. simple playlist on the right side
+2. playlist style from flowplayer on the left side
+
+== Changelog ==
+
+= 1.5.3 =
+* Allow comma in titles
+
+= 1.5.2 =
+* fix php error
+
+= 1.5.1 =
+* fix bug sometimes not initialising
+
+= 1.5 =
+* use youtube html5 player instead of flash player.
+
+= 1.01 =
+* updated readme.
+
+= 1.0 =
+* first
---
+++ simple-youtube.php
@@ -0,0 +1,115 @@
+<?php
+/*
+Plugin Name: Simple YouTube
+Plugin URI: http://roidayan.com
+Description: YouTube with playlist
+Version: 1.5.3
+Author: Roi Dayan
+Author URI: http://roidayan.com
+License: GPLv2
+*/
+
+class WPSimpleYouTube {
+
+    function __construct() {
+		$this->plugin_url = plugin_dir_url(__FILE__);
+        $this->settings = strtolower(__CLASS__.'_settings');
+
+        if (!is_admin()) {
+            add_action('init', array(&$this, 'add_styles'));
+            add_action('wp_enqueue_scripts', array(&$this, 'add_scripts'));
+            add_shortcode('youtube', array(&$this, 'shortcode'));
+        }
+    }
+
+    function shortcode($atts, $content = null) {
+        extract(shortcode_atts(array(
+            'width' => 400,
+            'height' => '',
+            'video' => '',
+            'side' => 'left',
+            'class' => '',
+            'vparams' => ''
+        ), $atts));
+
+        $r = '';
+        $playlist = null;
+
+        if (!empty($video)) {
+            if (!empty($content)) {
+                $content = strip_tags($content);
+                $content_lines = explode("\n", $content);
+                $playlist = array();
+                foreach ($content_lines as $line) {
+                    if (!empty($line)) {
+						$pos = strrpos( $line, ',' );
+						$playlist[] = array(
+							trim( substr( $line, 0, $pos ) ),
+							trim( substr( $line, $pos+1 ) )
+						);
+                    }
+                }
+            }
+
+            $r = $this->embed_player($video, intval($width), intval($height),
+                                     $playlist, $side, $class, $vparams);
+        }
+
+        return $r;
+    }
+
+    function add_styles() {
+        wp_enqueue_style('youtube-playlist', $this->plugin_url . 'inc/playlist.css', false, '1.0', 'all');
+    }
+
+    function add_scripts() {
+        wp_enqueue_script('jquery');
+		wp_enqueue_script('youtube-api', '//www.youtube.com/player_api', array('jquery'), '1.0', false);
+		wp_enqueue_script('youtube-playlist', $this->plugin_url . 'inc/youtube.playlist.js', array('youtube-api'), '1.0', false);
+		wp_enqueue_script( 'fitvids' , 'https://cdnjs.cloudflare.com/ajax/libs/fitvids/1.1.0/jquery.fitvids.min.js' , array('jquery'), '1.0' );
+    }
+
+    function embed_player($video, $width, $height, $playlist, $side, $class, $vparams) {
+        $salt = substr(md5(uniqid(rand(), true)), 0, 10);
+        $hash = __CLASS__.'_'. md5($video.$salt);
+        $hash_pl = $hash.'_pl';
+
+        $html = '<div id="' . $hash . '" class="ytplayer" data-video="'.$video.'"'.
+				' data-videow="'.$width.'" data-videoh="'.$height.'">';
+        // TODO vparams not being used
+		$script = '';
+
+        /* build playlist */
+        if (count($playlist) > 0) {
+            $playlist_div = '<div class="ytplaylist '.$class.'" style="float:'.$side.';"></div>';
+
+            $data = array();
+            foreach ($playlist as $m) {
+                $title = trim($m[0]);
+                $time = trim($m[1]);
+                $data[] = array(
+							'title' => $title,
+							'time' => $time
+						);
+            }
+
+			$json_data = json_encode($data);
+
+			$html .= $playlist_div;
+			$html .= <<<EOT
+<script>
+if (!window.ytplaylist)
+	window.ytplaylist = {};
+ytplaylist['$hash'] = $json_data;
+</script>
+EOT;
+
+        }
+
+        $html .= "</div>$script";
+
+        return $html;
+    }
+}
+
+new WPSimpleYouTube();
